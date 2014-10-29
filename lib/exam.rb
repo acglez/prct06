985565e600b7eed8c781a3ff283f776f304ca01d
Adrian <acglez10@gmail.com>
2014-10-29 17:22:03 +0000

    Creada la expectativa 'Debe existir una pregunta'. Falla.

--- lib/exam.rb
+++ lib/exam.rb
@@ -1,4 +1,6 @@
 require "exam/version"
+require "exam/Simple_Selection.rb"
+require "exam/list.rb"
 
 module Exam
   # Your code goes here...
--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 require 'exam'
 
- describe Exam do
-   
- end
+describe Exam do
+	describe "# Simple_Selection." do
+		
+		it "Debe existir una pregunta." do
+			@sS1.ask.should eq("¿Quien va a ser el balon de oro?")
+		end
+ 	end
+end
