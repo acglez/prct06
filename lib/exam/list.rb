7d89696f6fbe5b753820f2ca685e01668e8e8ad7
Adrian <acglez10@gmail.com>
2014-10-30 18:28:25 +0000

    Cumplida expectativa 'Se puede insertar un elemento'.

--- lib/exam/list.rb
+++ lib/exam/list.rb
@@ -9,4 +9,12 @@ class List
 	def pop()
       	@head = @head.next
    	end
+
+   	def add(value)
+        current = @head
+        while current.next != nil
+            current = current.next
+        end 
+        current.next = Node.new(value,nil)
+    end
 end
